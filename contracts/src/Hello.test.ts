c9d382dc487953bb67321ce7d77b959d562503c4
aeither <36173828+aeither@users.noreply.github.com>
2023-11-05 13:29:19 +0300

    clean

--- contracts/src/Hello.test.ts
+++
@@ -1,7 +0,0 @@
-// import { Hello } from './Hello';
-
-describe('Hello.js', () => {
-  describe('Hello()', () => {
-    it.todo('should be correct');
-  });
-});
--- contracts/src/index.ts
+++
@@ -1,4 +0,0 @@
-import { Add } from './Add.js';
-import { Hello } from './Hello.js';
-
-export { Add, Hello };
